d5b863e46d7688320c9c48895819542fa629f302
蔡荣庚 <89987112@qq.com>
2017-11-08 18:19:23 +0800

    v0.1

--- index.js
+++ index.js
@@ -4,44 +4,33 @@ function MyPlugin(options) {
 
 MyPlugin.prototype.apply = function (compiler) {
 
+  compiler.plugin('compilation', function(compilation) {
+
+    compilation.plugin('html-webpack-plugin-after-html-processing', function(htmlPluginData, callback) {
+      htmlPluginData.html = htmlPluginData.html.replace(/\/static\//g, "static/");
+      callback(null, htmlPluginData);
+    });
+  });
 
   compiler.plugin("emit", function (compilation, callback) {
 
 
     let ret = compilation.chunks.reduce((prev, cur) => {
-      let html = prev.html || [],
+      let
         css = prev.css || [];
 
 
       cur.files.forEach((item) => {
         if (/.css($|\?)/.test(item)) {
           css.push(item);
-        } else if (/.html($|\?)/.test(item)) {
-          html.push(item);
         }
       });
 
       return {
-        html,
         css
       }
     }, {});
 
-  // html 没法获取到,只能使用在config中配置publicPath 为 "./"
-    ret.html.forEach(function (filename) {
-      // source()可以得到每个文件的源码
-      let source = compilation.assets[filename].source(),
-        html = source.replace(/\/static\//g, "static/");
-
-      compilation.assets[filename] = {
-        source: function () {
-          return html;
-        },
-        size: function () {
-          return html.length;
-        }
-      }
-    });
 
 
     ret.css.forEach(function (filename) {
